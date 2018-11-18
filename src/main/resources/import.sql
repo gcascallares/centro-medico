a3cbfac484697d115da884ee8ee71a7ec5718871
ignacio <ignadario97@gmail.com>
2018-11-18 17:49:42 -0300

    usuario roles

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,7 +1,8 @@
 insert into especialidad (id,intervalo,nombreEspecialidad) values (1,30,'Cardiología'),(2,45,'Radiología'),(3,15,'Laboratorio');
 insert into medico (id,horaDesde,horaHasta,minutoDesde,minutoHasta,nombre,especialidad_id) values (1,8,16,0,30,'Juan Perez',1),(2,12,16,0,30,'Daniela Martinez',2),(3,9,12,0,0,'Ernesto Baba',3),(4,10,17,0,0,'Lucas Pratto',2);
 insert into consultorio (id,numero,medico_id) values (1,100,1),(2,101,2),(3,102,3),(4,103,2),(5,104,1),(6,105,null),(7,106,null);	
-insert into paciente (id, apellido, clave, dni, email, nombre) values (1,"ana", "1234", 111, "ala@", "alan"), (2,"brado", "0000", 222, "abrado@", "alan");
+insert into paciente (id, apellido, dni, nombre,usuario_id) values (1,"ana", 111, "alan",1), (2,"brado", 222, "alan",2);
 insert into diaslaborales (id,nombre) values (1,'Lunes'),(2,'Martes'),(3,'Miercoles'),(4,'Jueves'),(5,'Viernes');
 insert into dias_medicos(dia_id,medico_id) values (1,1),(3,1),(1,2),(2,2),(1,3),(5,3),(2,4),(3,4);
+insert into usuario(email,password,rol) values ('a@a.com','123456','paciente'),('b@b.com','123456','recepcionista');
 insert into turno (id, estado, fecha, horario, medico_id, paciente_id) values (1,"en_espera", "15-11-2018", "16:00", 1, 1), (2,"en_espera", "15-11-2018", "13:00", 1, 1),(3,"atendido", "16-11-2018", "16:00", 2, 1);
